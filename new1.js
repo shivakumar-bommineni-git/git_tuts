162cbe3919773f75be64cb0362447fc0c6954390
shivakumar <shivabommineni3@gmail.com>
2024-03-29 21:36:03 +0530

    shiva uploding new file to hub

---
+++ new1.js
@@ -0,0 +1,108 @@
+// function isPrime(num){
+//            if(num<=1)return false;
+//            for(let i=2;i<= num/2;i++){
+//             if(num % i ===0 )return false;
+//            } 
+//           return true;
+//     }
+//     for(let i=1;i<50;i++)
+//     {if(isPrime(i))console.log(`${i} is prime`)}
+    
+//     var x;
+//     console.log(x);
+//     x = 10
+    
+    
+//     /* name="raju";
+//     let name;
+//     console.log(name);
+//     */
+    
+//     var arr = [4,9,13,16,28,37];
+//     var val = arr.find(myFunction);
+//     var index = arr.findIndex(myFunction);
+//     document.write(value + " at Index :" +index);
+    
+//     function myFunction(value,index,array){
+//           return value>18;
+//     }
+    
+    
+    
+//     const str = "hello";
+//     const arr = Array.from(str);
+    
+//     console.log(arr); // Output: ["h", "e", "l", "l", "o"]
+    
+    
+    
+//     function Perfect(){
+//         var n = parseInt(document.getElementById("n").value);
+//         var sum = 0;
+//         for(var i=1;i<n;i++){
+//             if(n%i===0)sum+=i;
+    
+//     }
+//     if(n == sum){
+//         document.write("The number "+n+" is a perfect number");
+//     }else{
+//         document.write("The number "+n+" is not a perfect number");
+//     }
+//     }
+//     console.log(Perfect(5));
+    
+    
+    
+//     var arr = [5,9,5,9,20,34];
+//     for (let i of arr) {
+//         if (i % 2 === 0) {
+//             console.log(i);
+//         }
+//      }
+    
+    
+    
+//     var arr = [5,9,5,9,20,34];
+//     for(var i=1;i<arr.length;i++){
+//         if(arr[i]>arr[i-1]){
+//             console.log('increasing');
+//         } else if(arr[i]==arr[i-1]){
+//             console.log('same')
+//         } else {
+//             console.log('decreasing');
+//     }
+//     }
+    
+    
+    
+//     var name = "amar";
+//     for(var i=0;i<name.length;i++){
+//         console.log(name[i]);
+//     }
+    
+    
+//     var name = "amar";
+//     for(var i=name.length-1;i>0;i--){
+//         console.log(name[i]);
+//     }
+    
+    
+    
+//     var line = "this is online web  programming course by amar sir from india";
+//     var arr = line.split(" ");
+//     console.log("words count",arr.length);
+//     console.log(arr.lastIndexOf(line.length-1));
+    
+//     const array = [1, 2, 3, 4, 2, 5, 2];
+//     const lastIndex = array.lastIndexOf(2); // Returns 5
+//     console.log(`The last index of the element '2' in the array is ${lastIndex}`);
+    
+    
+//     var name = prompt('Enter your Name');
+//     var age =  prompt('Enter  Your Age');
+//     agedays = age*3656.25;
+//     remainingdays = (60-age)*3656.25;
+//     alert('hello'+name+"...\nYour current age:"+agedays+"remainig days"+remainingdays)
+    
+    
+    
